e442203924620fa22a96f25059480869917328a5
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-07-28 12:58:16 -0700

    added navigation

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -6,6 +6,7 @@ const Missing = () => {
         <main className='Missing'>
             <h2>Page Not Found</h2>
             <p>Well, that's disappointing.</p>
+            {/*navigate paragraph to visit our home page once the user login to the website*/}
             <p>
                 <Link to='/'>Visit Our Homepage</Link>
             </p>
